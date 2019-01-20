082a0658b0d57fa3257a16fc7a81167444fd1144
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-20 11:36:58 -0800

    1.0.36: simplerpatterntypenames

--- src/patterns.ts
+++ src/patterns.ts
@@ -1,16 +1,16 @@
-import { PatternMaterial } from '@musical-patterns/compiler'
-import { PatternMetadata } from '@musical-patterns/pattern'
+import { Material } from '@musical-patterns/compiler'
+import { Metadata } from '@musical-patterns/pattern'
 import { buildPatterns, PatternFor, PatternId, Patterns } from '@musical-patterns/registry'
 import { buildEntities, buildScales } from './materials'
 import { post } from './metadata'
 import { specData, XenharmonicSeriesSpec } from './specs'
 
-const material: PatternMaterial = {
+const material: Material = {
     buildEntitiesFunction: buildEntities,
     buildScalesFunction: buildScales,
 }
 
-const metadata: PatternMetadata = {
+const metadata: Metadata = {
     description: post,
     formattedName: 'Xenharmonic Series',
     mostRecentPublish: process.env.PUBLISH_DATE || '2016-09-20T07:00:00.000Z',
