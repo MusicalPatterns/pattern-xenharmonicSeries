d78bc590ce286ac06c312e7db29bc29068b3aff1
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-29 11:57:41 -0800

    initial

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,16 @@
 // tslint:disable:no-magic-numbers
 
-import { Frequency, Index, to } from '@musical-patterns/utilities'
+import { Frequency, Index, Power, to } from '@musical-patterns/utilities'
 
-const XENHARMONIC_SERIES_LOWER_BOUND: Index = to.Index(1)
-const XENHARMONIC_SERIES_UPPER_BOUND: Index = to.Index(32)
+const XENHARMONIC_SERIES_INITIAL_LOWER_BOUND: Index = to.Index(1)
+const XENHARMONIC_SERIES_INITIAL_UPPER_BOUND: Index = to.Index(32)
 
-const XENHARMONIC_SERIES_BASE_FREQUENCY: Frequency = to.Frequency(220)
+const XENHARMONIC_SERIES_INITIAL_BASE_FREQUENCY: Frequency = to.Frequency(220)
+const XENHARMONIC_SERIES_INITIAL_POWER: Power = to.Power(-1)
 
 export {
-    XENHARMONIC_SERIES_LOWER_BOUND,
-    XENHARMONIC_SERIES_UPPER_BOUND,
-    XENHARMONIC_SERIES_BASE_FREQUENCY,
+    XENHARMONIC_SERIES_INITIAL_LOWER_BOUND,
+    XENHARMONIC_SERIES_INITIAL_UPPER_BOUND,
+    XENHARMONIC_SERIES_INITIAL_BASE_FREQUENCY,
+    XENHARMONIC_SERIES_INITIAL_POWER,
 }
