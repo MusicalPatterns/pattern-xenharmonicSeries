19f4072a8febb752f438ec3dcc0c42ff2787f994
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-29 14:57:38 -0800

    1.0.67: ms

--- src/material/scales.ts
+++ src/material/scales.ts
@@ -12,7 +12,7 @@ const buildScales: BuildScalesFunction =
         const gainScale: Scale = nonScale
         const durationsScale: Scale = {
             offset: spec[ StandardSpecProperties.DURATION_OFFSET ] || to.Offset(0),
-            scalar: to.Scalar(from.Millisecond(spec[ StandardSpecProperties.BASE_DURATION ] || to.Millisecond(1))),
+            scalar: to.Scalar(from.Milliseconds(spec[ StandardSpecProperties.BASE_DURATION ] || to.Milliseconds(1))),
             scalars: nonScale.scalars,
         }
         const pitchesScale: Scale = {
