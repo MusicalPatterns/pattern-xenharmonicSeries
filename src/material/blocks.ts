d5f78b8fc7fca4289e829175a98854d7d4d824e6
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-05 09:52:52 -0800

    1.0.74: integer

--- src/material/blocks.ts
+++ src/material/blocks.ts
@@ -5,7 +5,7 @@ import { XenharmonicSeriesSpec } from '../spec'
 const buildBlock: (spec: XenharmonicSeriesSpec, stackIndex?: Index) => Block =
     (spec: XenharmonicSeriesSpec, stackIndex: Index = to.Index(0)): Block =>
         to.Block(buildBoundedNumbers(INITIAL, to.Index(from.Time(duration(spec)) - 1))
-            .map((n: number) => apply.Offset(n, to.Offset(from.Index(stackIndex)))))
+            .map((blockElement: number) => apply.Offset(blockElement, to.Offset(from.Index(stackIndex)))))
 
 export {
     buildBlock,
