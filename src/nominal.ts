8bf2995b86535a9b3c9c42dabc408b54aee13a25
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-25 18:58:16 -0800

    1.0.99: use new nominal helper

---
+++ src/nominal.ts
@@ -0,0 +1,20 @@
+// tslint:disable ban-types no-type-definitions-outside-types-modules no-any
+
+import { buildNominalInterface, DUMMY_VALUE_FOR_BUILDING_NOMINAL_INTERFACE } from '@musical-patterns/utilities'
+
+type PartialSumOrProduct = Number & { _PartialSumOrProductBrand: void }
+type Term = Number & { _TermBrand: void }
+
+const { to, from } = buildNominalInterface({
+    number: {
+        PartialSumOrProduct: DUMMY_VALUE_FOR_BUILDING_NOMINAL_INTERFACE as any as PartialSumOrProduct,
+        Term: DUMMY_VALUE_FOR_BUILDING_NOMINAL_INTERFACE as any as Term,
+    },
+})
+
+export {
+    to,
+    from,
+    PartialSumOrProduct,
+    Term,
+}
